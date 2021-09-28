67a9057109577e39534722422a27e35b2c15f7a0
adegbola.babs <adegbola.babs@GMAIL.COM>
2021-09-28 10:07:44 -0400

    Initial commit

---
+++ Api/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+from rest_framework import status
+from .serializer import TaskSerializer
+from .models import Task
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls= {
+        'List': '/task-list/',
+        'Detail':'/task-list/<str:pk>',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>',
+        'Delete': '/task-delete/<str:pk>',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=tasks,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DElETE'])
+def taskDelete(request,pk):
+    tasks = Task.objects.get(id=pk)
+    tasks.delete()
+    return Response('done')
+
